37935e68fe4b597eb0f986d2a7faf0b911e05197
MicheleSpecchia <michele.specchia@edu.unife.it>
2023-08-12 16:40:26 +0200

    final

--- img/pagina_files/script.js
+++
@@ -1,9 +0,0 @@
-function showP(image) { 
-    document.getElementById('sp').style.backgroundImage = 'linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)), url(' + image +') ';
-};
-function changeT(text){
-    document.getElementById('name').innerHTML = text
-}
-function changeD(newText) {
-    document.getElementById('desc').innerHTML = newText;
-}
